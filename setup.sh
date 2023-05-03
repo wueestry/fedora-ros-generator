873fc208ee79324615f4801ba80672fd6897c1b6
Ryan Wüest <ryan.wueest@protonmail.com>
2023-05-03 16:14:24 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#! etc/bin/bash
+sudo dnf install python3-devel python3-pygraphviz python3-defusedxml python3-rosdep python3-rosinstall_generator python3-copr python3-termcolor python3-jinja2 rpmdevtools
+
+sudo rosdep init
+rosdep update
